52dae6a1b602fb861c43f317fe430c28e4b8cbb4
vagrant <vagrant@homestead>
2017-11-21 23:10:04 +0000

    Add country to table hotels

---
+++ database/migrations/2017_11_21_230655_add_country_to_hotels_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCountryToHotelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('hotels', function($table) {
+      $table->string('country')->after('zip_code');
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('hotels', function($table) {
+      $table->dropColumn('country');
+      });
+    }
+}
