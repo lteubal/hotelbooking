b3bad96d4841aa666987dcc068274135272774fd
vagrant <vagrant@homestead>
2017-11-22 02:25:24 +0000

    Create rooms table and migration

---
+++ database/migrations/2017_11_22_021320_create_rooms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('hotel_id')->unsigned();
+            $table->smallInteger('occupancy')->unsigned();
+            $table->text('conditions');
+            $table->text('cancellation_policy');
+            $table->string('room_type', 60);
+            $table->string('room_view', 60);
+            $table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
