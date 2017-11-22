3071b839d77c8a4427e3ff223b8efa123c3e6e1d
vagrant <vagrant@homestead>
2017-11-22 13:19:36 +0000

    add routes to all rooms and individual room

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,5 @@
 
 Route::get('/', 'HomeController@index');
 Route::get('/search', 'HomeController@search');
+Route::get('/rooms', 'HomeController@rooms');
+Route::get('/room', 'HomeController@room');
